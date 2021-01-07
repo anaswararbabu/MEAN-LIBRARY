81cd7d1d3ea428fdfd328ec143a5bc158fd66812
anaswararbabu <74505032+anaswararbabu@users.noreply.github.com>
2021-01-07 12:01:15 +0530

    Update authordata.js

--- Backend/src/model/authordata.js
+++ Backend/src/model/authordata.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-//  mongoose.connect('mongodb://localhost:27017/MeanLibrary');
-mongoose.connect('mongodb+srv://usertwo:usertwo@library-mean.lrajk.mongodb.net/LIBRARY-APP?retryWrites=true&w=majority');
+ mongoose.connect('mongodb://localhost:27017/MeanLibrary');
+// mongoose.connect('mongodb+srv://usertwo:usertwo@library-mean.lrajk.mongodb.net/LIBRARY-APP?retryWrites=true&w=majority');
 const Schema = mongoose.Schema;
 
 var NewauthorSchema = new Schema({
